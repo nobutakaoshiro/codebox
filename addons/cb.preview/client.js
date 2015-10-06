f6d9c48e7351e47f39d8ebb2d6418ba057808f8c
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2015-10-06 18:54:47 +0900

    avoid popup block

--- addons/cb.preview/client.js
+++ addons/cb.preview/client.js
@@ -4,6 +4,9 @@ define([
     var menu = codebox.require("core/commands/menu");
     var rpc = codebox.require("core/backends/rpc");
 
+    var previewUrl = "javascript:alrt('no preview')";
+
+
     // Add menu
     menu.register("preview", {
         title: "Preview",
@@ -14,17 +17,16 @@ define([
             'category': "Preview",
             'title': "Preview with New Window",
             'description': "Preview with New Window",
-            'action': function() {
-                return rpc.execute("preview/url").then(function(url) {
-                    return window.open(url);
-                });
+            'action': function(event) {
+                window.open(previewUrl);
             }
         }
     ]);
 
     // Open changes if version changes
     app.once("ready", function() {
-        //  Show release not if version increased
-        //  If first time show welcome message
+        rpc.execute("preview/url").then(function(url) {
+            previewUrl = url;
+        });
     });
 });
